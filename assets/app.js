75912c51021f36f225e3ed65d137d6e033ede485
Maxim Sobin <maxim.sobin@gmail.com>
2024-05-24 00:48:15 +0500

    Refactoring

---
+++ assets/app.js
@@ -0,0 +1,19 @@
+import { registerVueControllerComponents } from '@symfony/ux-vue';
+import './bootstrap.js';
+
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.scss';
+
+const $ = require('jquery');
+require('bootstrap');
+
+require('leaflet');
+
+registerVueControllerComponents(require.context('./vue/controllers', true, /\.vue$/));
---
+++ assets/vue/controllers/Demo.vue
@@ -0,0 +1,130 @@
+<template>
+</template>
+
+<script>
+import {createApp} from 'vue';
+import axios from 'axios';
+
+const app = createApp({
+    data() {
+        return {
+            isWannaDrinkDisabled: false,
+            me: {
+                name: 'test',
+                description: "description",
+                latitude: 51.14607,
+                longitude: 71.420922,
+            },
+            token: '',
+            marker: null,
+            map: null,
+            matesMarkers: {},
+        }
+    },
+    mounted() {
+        this.initMap();
+    },
+    methods: {
+        async wannaDrink() {
+            const response = await axios.post('/api/v1/wanna-drink', this.me)
+                .catch(function (error) {
+                    console.log(error);
+                })
+
+            this.token = response.data.token;
+            axios.defaults.headers.common = {'Authorization': `Bearer ${this.token}`}
+            this.isWannaDrinkDisabled = true;
+            this.map.off('click');
+
+            await this.findNearBy();
+
+            setInterval(() => {
+                this.findNearBy();
+            }, 5000)
+        },
+        async findNearBy() {
+            const response = await axios.get('/api/v1/mates/nearby',)
+                .catch(function (error) {
+                    console.log(error);
+                })
+
+            response.data.forEach((mate) => {
+                if (this.matesMarkers[mate.id]) {
+                    return;
+                }
+
+                this.matesMarkers[mate.id] = new L.marker([mate.point.latitude, mate.point.longitude], {icon: this.getIcon()})
+                    .addTo(this.map)
+                    .bindPopup(mate.name + '<br>' + mate.description + '<br><br>'
+                        + '<input type="button" onclick="alert(123)" class="send-message-btn" value="Send message" data-id="' + mate.id + '">');
+            })
+
+            let mateIds = response.data.map((mate) => mate.id);
+            for (let mateId in this.matesMarkers) {
+                if (!mateIds.includes(mateId)) {
+                    this.map.removeLayer(this.matesMarkers[mateId]);
+                    delete this.matesMarkers[mateId];
+                }
+            }
+        },
+        initMap() {
+            let config = {
+                minZoom: 7,
+                maxZoom: 18,
+            };
+
+            const zoom = 16;
+            const lat = 51.14607;
+            const lng = 71.420922;
+
+            this.map = L.map("map", config).setView([lat, lng], zoom);
+
+            L.tileLayer(
+                "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+                {
+                    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+                }
+            ).addTo(this.map);
+
+            this.map.on('click', (e) => {
+                this.me.latitude = e.latlng.lat;
+                this.me.longitude = e.latlng.lng;
+
+                if (this.marker) {
+                    this.map.removeLayer(this.marker);
+                }
+
+                this.marker = new L.marker(e.latlng, {icon: this.getIcon('red')}).addTo(this.map).bindPopup('me');
+            });
+        },
+        getIcon(color) {
+            let colors = ['blue', 'gold', 'green', 'orange', 'yellow', 'violet', 'grey', 'black'];
+
+            if (color === undefined) {
+                color = colors[Math.floor(Math.random() * colors.length)];
+            }
+
+            return new L.Icon({
+                iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-' + color + '.png',
+                shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+                iconSize: [25, 41],
+                iconAnchor: [12, 41],
+                popupAnchor: [1, -34],
+                shadowSize: [41, 41]
+            });
+        },
+        sendMessage(event) {
+        }
+    },
+    watch: {
+        me: {
+            deep: true,
+            handler() {
+                this.isWannaDrinkDisabled = !(this.me.name && this.me.latitude && this.me.longitude && !this.me.token)
+            },
+        }
+    },
+})
+
+app.mount('#app')
+</script>
